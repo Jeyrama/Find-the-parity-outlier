395c444efbba4941088bcfad0f153c57b4113a68
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-03 18:36:07 -0700

    Update Find-the-parity-outlier.js

--- Find-the-parity-outlier.js
+++ Find-the-parity-outlier.js
@@ -15,3 +15,13 @@ Should return: 160 (the only even number)
 
 
 // Solution
+
+function findOutlier(integers) {
+  return integers.slice(0,3).filter(even).length >= 2 ? integers.find(odd) : integers.find(even);
+}
+function even(num) {
+  return (num % 2 == 0);
+}
+function odd(num) {
+  return !even(num)
+}
